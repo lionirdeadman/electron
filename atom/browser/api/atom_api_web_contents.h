e4332842bbef861842aa9f95a973ebb7ce85989f
Andy Dill <andy.dill@gmail.com>
2019-02-04 12:23:33 -0800

    add drag & drop support for osr

--- atom/browser/api/atom_api_web_contents.h
+++ atom/browser/api/atom_api_web_contents.h
@@ -22,6 +22,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_binding_set.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/drop_data.h"
 #include "content/public/common/favicon_url.h"
 #include "electron/atom/common/api/api.mojom.h"
 #include "electron/buildflags/buildflags.h"
@@ -319,6 +320,13 @@ class WebContents : public mate::TrackableObject<WebContents>,
 
   bool EmitNavigationEvent(const std::string& event,
                            content::NavigationHandle* navigation_handle);
+
+  bool start_dragging = false;
+  bool dragging = false;
+  content::DropData drop_data;
+  blink::WebDragOperationsMask drag_ops = blink::kWebDragOperationNone;
+  gfx::ImageSkia drag_image;
+  gfx::Vector2d drag_image_offset;
 
  protected:
   // Does not manage lifetime of |web_contents|.
